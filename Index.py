ad30acda352da4b87fa41c00e8e36ec38c1456ba
Marcelo Cabral Ghilardi <kabrau@gmail.com>
2017-08-05 00:08:09 -0300

    version

--- Index.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-import time as t
-
-import numpy as np
-from numpy import array, arange, uint8 
-
-print("OpenCV Version : %s " % cv2.__version__) 
---
+++ versao.py
@@ -0,0 +1,3 @@
+import cv2
+
+print("OpenCV Version : %s " % cv2.__version__) 
